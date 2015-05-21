21a75b501263ad03a908df544fabd3c075d856c0
addisonbean <addisonbean@gmail.com>
2015-05-21 13:01:45 -0500

    The Event class now uses the ekEvent more

--- Koolendar/Event.swift
+++ Koolendar/Event.swift
@@ -7,17 +7,36 @@
 //
 
 import Foundation
+import EventKit
 
 class Event {
     
+    let ekEvent = EKEvent()
+    
+    var title: String {
+        get { return self.ekEvent.title }
+        set { self.ekEvent.title = newValue }
+    }
+    
+    var startDate: NSDate {
+        get { return self.ekEvent.startDate }
+        set { self.ekEvent.startDate = newValue }
+    }
+    
+    var endDate: NSDate {
+        get { return self.ekEvent.endDate }
+        set { self.ekEvent.endDate = newValue }
+    }
+    
+    var allDay: Bool {
+        get { return self.ekEvent.allDay }
+        set { self.ekEvent.allDay = newValue }
+    }
+    
     var id: Int?
-    var title: String
-    var startDate: NSDate
-    var endDate: NSDate?
-    var allDay = false
     
     init(title: String, startDate: NSDate, endDate: NSDate?) {
-    
+        
         self.title = title
         self.startDate = startDate
         if let endDate = endDate {
@@ -32,22 +51,40 @@ class Event {
     func save() {
         self.id = Event.all.count // this may not be compatable with EKEvents, idk
         
+        Event.eventStore.saveEvent(self.ekEvent, span: EKSpanThisEvent, commit: true, error: nil)
+        
         // save what we can in an EKEvent, and put the rest into the db
     }
     
     // ugly workaround for class vars below
     
+    class func requestEventKitPermission() {
+        Event.eventStoreAccess = EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent)
+        if Event.eventStoreAccess == .NotDetermined {
+            Event.eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion: {
+                gotAccess, error in
+            })
+        }
+    }
+    
     private struct ClassVariables {
         static var all: [Event] = []
+        static let eventStore = EKEventStore()
+        static var eventStoreAccess: EKAuthorizationStatus!
     }
     
     class var all: [Event] {
-        get {
-        return ClassVariables.all
-        }
-        set {
-            ClassVariables.all = newValue
-        }
+        get { return ClassVariables.all }
+        set { ClassVariables.all = newValue }
+    }
+    
+    class var eventStore: EKEventStore {
+        get { return ClassVariables.eventStore }
+    }
+    
+    class var eventStoreAccess: EKAuthorizationStatus {
+        get { return ClassVariables.eventStoreAccess }
+        set { ClassVariables.eventStoreAccess = newValue }
     }
     
 }
