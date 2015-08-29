17467ce237f955dd61669f917f271bd999cc1ffd
addisonbean <addisonbean@gmail.com>
2015-08-28 20:10:04 -0500

    UUUuugggghhhhhhh

--- Koolendar/SettingsSubViewController.swift
+++ Koolendar/SettingsSubViewController.swift
@@ -16,36 +16,8 @@ class SettingsSubViewController: CenterViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadData()
         tableView.dataSource = self
-    }
-    
-    func loadData(_ tryAgainOnFailure: Bool = true) -> Bool {
-        var success = false
-        
-        if let path = NSBundle.mainBundle().pathForResource("settings.json", ofType: "json") {
-            if let data = NSData(contentsOfFile: path, options: NSDataReadingOptions(), error: nil) {
-                if let allSettings = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(), error: nil) as? [String: [String: Any]] {
-                    success = true
-                    settings = allSettings[self.settingName]
-                    return true
-                }
-            }
-        }
-        
-        if !success && tryAgainOnFailure {
-            // if this is reached that means the settings are corrupt or don't exist, so we'll just load the default settings
-            // but I feel like we should also backup the old one if it was corrupted
-            let source = NSBundle.mainBundle().pathForResource("default_settings", ofType: "json")!
-            
-            let dir = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
-            let dest = dir.stringByAppendingPathComponent("settings.json")
-            
-            NSFileManager.defaultManager().copyItemAtPath(source, toPath: dest, error: nil)
-            return loadData(false)
-        }
-        
-        return false
+        println(self.settings)
     }
     
     @IBAction func goBack() {
--- Koolendar/SettingsViewController.swift
+++ Koolendar/SettingsViewController.swift
@@ -9,14 +9,53 @@
 import UIKit
 
 class SettingsViewController: CenterViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    let settings = ["Notifications", "Color Scheme"]
 
     @IBOutlet weak var tableView: UITableView!
     
+    var settings: [String: [String: Any]]!
+    
     override func viewDidLoad() {
+        println(loadData())
         self.tableView.dataSource = self
         self.tableView.delegate = self
+    }
+    
+    func loadData(_ tryAgainOnFailure: Bool = true) -> Bool {
+        var success = false
+        
+        if let path = NSBundle.mainBundle().pathForResource("settings.json", ofType: "json") {
+            if let data = NSData(contentsOfFile: path, options: NSDataReadingOptions(), error: nil) {
+                if let _settings = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(), error: nil) as? [String: [String: Any]] {
+                    success = true
+                    self.settings = _settings
+                    return true
+                }
+            }
+        }
+        
+        if !success && tryAgainOnFailure {
+            // if this is reached that means the settings are corrupt or don't exist, so we'll just load the default settings
+            // but I feel like we should also backup the old one if it was corrupted
+            let source = NSBundle.mainBundle().pathForResource("default_settings", ofType: "json")!
+            
+//            let dir = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
+//            let dest = dir.stringByAppendingPathComponent("settings.json")
+            let dest = NSBundle.mainBundle().resourcePath!.stringByAppendingPathComponent("settings.json")
+            
+            println(source)
+            println(dest)
+            
+            var error: NSError?
+            NSFileManager.defaultManager().copyItemAtPath(source, toPath: dest, error: &error)
+            
+            if let error = error {
+                println(error)
+            }
+            
+            return loadData(false)
+        }
+        
+        return false
     }
     
     @IBAction func showSidebar() {
@@ -29,14 +68,15 @@ class SettingsViewController: CenterViewController, UITableViewDataSource, UITab
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("SettingsTableViewCell", forIndexPath: indexPath) as! SettingsTableViewCell
-        cell.title.text = settings[indexPath.row]
+        cell.title.text = settings.keys.array[indexPath.row]
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let vc = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier("SettingsSubViewController") as! SettingsSubViewController
         vc.delegate = self.delegate
-        vc.settingName = self.settings[indexPath.row]
+        vc.settingName = self.settings.keys.array[indexPath.row]
+        vc.settings = self.settings[vc.settingName]
         self.navigationController!.pushViewController(vc, animated: true)
     }
     
