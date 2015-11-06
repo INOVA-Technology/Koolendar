788d568d46e5e800359fd9f6f6e976c7a2fe28cc
addisonbean <addisonbean@gmail.com>
2015-11-05 18:15:23 -0600

    Fixed the crashing on physical devices

---
+++ Koolendar/ReminderForm.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderForm.swift
+//  Koolendar
+//
+//  Created by Addison Bean on 11/2/15.
+//  Copyright © 2015 INOVA. All rights reserved.
+//
+
+import UIKit
+
+class ReminderForm: UIViewController {
+    
+//    @IBAction createReminder() {
+//    
+//    }
+    
+}
